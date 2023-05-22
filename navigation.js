5dc3a82c1e163290cbf741ef45ac1005cc0d0e6c
Adomelism <adomelism@gmail.com>
2023-05-22 15:19:59 +0300

    updated-navigation

--- navigation.js
+++ navigation.js
@@ -5,7 +5,59 @@
 //   4.4. Posts / pranešimų puslapis.
 //   4.5. Pakeisti aktyvaus puslapio nuorodos stilių.
 
-const navigationElement = document.createElement('nav');
-  navigationElement.textContent = 'Labas';
+function navigation() {
+    const headerElement = document.createElement('header');
+    const navigationElement = document.createElement('nav');
+    navigationElement.classList.add('main-navigation');
+    headerElement.append(navigationElement);
 
-  document.body.prepend(navigationElement);
+    const menuList = document.createElement('ul');
+    menuList.classList.add('menu', 'main-menu');
+    navigationElement.append(menuList);
+
+
+    const menuItems = [
+    {
+        title: 'Home',
+        path: 'home.html',
+    },
+    {
+        title: 'Users',
+        path: 'users.html',
+    },
+    {
+        title: 'Albums',
+        path: 'albums.html',
+    },
+    {
+        title: 'Posts',
+        path: 'posts.html',
+    }
+    ];
+
+    menuItems.forEach(item => {
+        let {title, path} = item;
+
+        const menuItem = document.createElement('li');
+        menuList.append(menuItem)
+
+        const menuLink = document.createElement('a');
+
+        if (location.pathname === '/' + path) {
+            menuLink.classList.add('active');
+        }
+
+    
+
+        menuItem.append(menuLink);
+        menuLink.href = path;
+        menuLink.textContent = title;
+
+
+    })
+
+    document.body.prepend(headerElement);
+
+}
+
+navigation();
