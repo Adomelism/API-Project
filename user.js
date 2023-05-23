23c5a2bded4e03479e6173b249a94f41a70bbbac
Adomelism <adomelism@gmail.com>
2023-05-23 21:44:44 +0300

    postsandalbums

--- user.js
+++ user.js
@@ -1,16 +1,9 @@
-
-// 6. Šiame puslapyje (user.html) turi būti atvaizduojama:
-//   6.1. Visi vartotojo parašyti įrašai (posts). Kiekvienas post'as turi turėti nuorodą.
-//   6.2. Visi vartotojo sukurti foto albumai. Kiekvienas albumas turės pavadinimą, kuris turi būti nuoroda.
-
 const usersListSection = document.querySelector('#content')
 
 
 async function userData() {
-    const res = await fetch ('https://jsonplaceholder.typicode.com/users/1')
+    const res = await fetch ('https://jsonplaceholder.typicode.com/users/1?_embed=posts&_embed=albums')
     const userInfo = await res.json()
-
-    console.log(userInfo)
 
     const name = userInfo.name
     const nick = userInfo.username
@@ -27,7 +20,10 @@ async function userData() {
     nickName.textContent = `(Nickname: ${nick})`
     
     const userEmail = document.createElement('h4')
-    userEmail.textContent = `Email: ${email}`;
+    const emailLink = document.createElement('a');
+    userEmail.append(emailLink);
+    emailLink.href = `mailto: ${email}`
+    emailLink.textContent = `Email: ${email}`;
 
     const userAddress = document.createElement('h4')
     const addressLink = document.createElement('a')
@@ -36,15 +32,62 @@ async function userData() {
     userAddress.append(addressLink)
     
     const userPhone = document.createElement('h4')
-    userPhone.textContent = `Phone: ${phone}`;
+    const phoneLink = document.createElement('a');
+    userPhone.append(phoneLink);
+    phoneLink.href = `tel:${phone}`;
+    phoneLink.textContent = `Phone: ${phone}`;
 
     const userWebsite = document.createElement('h4')
-    userWebsite.textContent = `Website: ${website}`;
+    const websiteLink = document.createElement('a')
+    userWebsite.append(websiteLink)
+    websiteLink.href = `https://www.${website}`
+    websiteLink.textContent = `Website: ${website}`;
 
     const userCompany = document.createElement('h4')
     userCompany.textContent = `Company: ${company}`;
     
     
-    usersListSection.append(userName, nickName, userEmail, userAddress, userPhone, userWebsite, userCompany);
+    usersListSection.prepend(userName, nickName, userEmail, userAddress, userPhone, userWebsite, userCompany);
 }
 userData();
+
+async function userPosts() {
+    const res = await fetch ('https://jsonplaceholder.typicode.com/users/1?_embed=posts&_embed=albums')
+    const userInfo = await res.json()
+
+    const postitle = document.createElement('h2')
+    postitle.textContent = `Posts:`
+
+    userInfo.posts.forEach(post => {
+        console.log(post.title)
+        const postList = document.createElement('p')
+        const postLink = document.createElement('a')
+        postLink.href = `./post.html`
+        postLink.textContent = post.title
+
+        usersListSection.append(postitle, postList, postLink)
+    });
+
+}
+userPosts();
+
+async function userAlbums() {
+    const res = await fetch ('https://jsonplaceholder.typicode.com/users/1?_embed=posts&_embed=albums')
+    const userInfo = await res.json()
+
+    const albumTitle = document.createElement('h2')
+    albumTitle.textContent = `Albums:`
+
+    userInfo.albums.forEach(album => {
+        const albumList = document.createElement('p')
+        const albumLink = document.createElement('a')
+        albumLink.href = `./album.html`
+        albumLink.textContent = album.title
+
+        usersListSection.append(albumTitle, albumList, albumLink)
+
+    });
+
+    
+}
+userAlbums();
