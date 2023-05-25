82bb0aa319ec744ab12ad58b0c7789899e048df5
Adomelism <adomelism@gmail.com>
2023-05-25 08:10:31 +0300

    commit last day changes

---
+++ album.js
@@ -0,0 +1,63 @@
+//   Panaudoti library (biblioteką), kuri skirta gražiam galerijos atvaizdavimui, pvz.:
+//     8.3.1. https://photoswipe.com/
+//     8.3.2. https://nanogallery2.nanostudio.org/
+//     8.3.3. https://sachinchoolur.github.io/lightgallery.js/
+//     8.3.4. Arba bet kurią kitą.
+
+async function init() {
+    const contentElement = document.querySelector('#content')
+
+    const queryParams = location.search;
+    const urlParams = new URLSearchParams(queryParams);
+    const id = urlParams.get('album_id');
+
+    console.log(id)
+
+    // if (!id) {
+    //     contentElement.innerHTML = `<h1>Something went wrong...</h1>
+    //                                 <p>Search for more albums <a href="./albums.html">here</a>.</p>`
+    //     return;
+    //   }
+
+    // const res = await fetch(`https://jsonplaceholder.typicode.com/albums/${id}?_expand=user&_embed=photos`)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/albums/1?_expand=user&_embed=photos`)
+    const albumData = await res.json()
+
+    const albumWrapperElement = createAlbum(albumData)
+
+    contentElement.append(albumWrapperElement)
+}
+
+init();
+
+function createAlbum(albumData) {
+    const albumWrapper = document.createElement('div')
+    albumWrapper.classList.add('album-wrapper')
+
+    const albumContent = document.createElement('div')
+    albumContent.classList.add('album-content')
+
+    const albumTitle = document.createElement('h1')
+    albumTitle.textContent = albumData.title
+    
+    const albumAuthor = document.createElement('a')
+    albumAuthor.href = `./users.html/user_id=` + albumData.user.id
+    albumAuthor.textContent = albumData.user.name
+
+    albumContent.append(albumTitle, albumAuthor)
+
+    const albumPhotoList = document.createElement('div')
+    albumPhotoList.classList.add('album-photo-list')
+
+    albumData.photos.forEach(photo => {
+        const photoList = document.createElement('img')
+        photoList.src = photo.thumbnailUrl
+
+        albumPhotoList.append(photoList)
+
+    });
+
+    albumWrapper.append(albumContent, albumPhotoList)
+    return albumWrapper;
+}
+
--- navigation.js
+++ navigation.js
@@ -1,3 +1,10 @@
+// 9. Sukurti paieškos funkcionalumą navigacijos elemente:
+// 9.1. Navigacijos elemente sukurti formą, kuri turi text tipo input elementą (nepamiršti pridėti name atributą).
+// 9.2. Formos submit metu, naudojant action atributą, nukreipti į naują puslapį (search.html).
+// 9.3. Šiame puslapyje atvaizduoti paieškos rezultatą.
+// 9.3.1. Jeigu nėra tinkamų rezultatų, tai parašyti jog rezultatų pagal užklausą nerasta.
+
+
 function navigation() {
     const headerElement = document.createElement('header');
     const navigationElement = document.createElement('nav');
@@ -7,7 +14,6 @@ function navigation() {
     const menuList = document.createElement('ul');
     menuList.classList.add('menu', 'main-menu');
     navigationElement.append(menuList);
-
 
     const menuItems = [
     {
@@ -40,12 +46,9 @@ function navigation() {
             menuLink.classList.add('active');
         }
 
-    
-
         menuItem.append(menuLink);
         menuLink.href = path;
         menuLink.textContent = title;
-
 
     })
 
